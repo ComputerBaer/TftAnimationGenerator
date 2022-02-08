0c5df0feaf84fb733733a67dd9acad5f5f93f682
Dominik Strebinger <dominik.strebinger@daenarion.com>
2022-02-08 15:11:20 +0100

    feat: add avalonia mvvm application

---
+++ src/TftAnimationGenerator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TftAnimationGenerator.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
